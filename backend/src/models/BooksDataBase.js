95582ed24078763073c859b01c13d92e41750433
Daniel Rodrigues <danielrodrigues0709@gmail.com>
2019-12-16 18:11:05 -0300

    Backend commited

--- backend/src/models/BooksDataBase.js
+++ backend/src/models/BooksDataBase.js
@@ -4,13 +4,16 @@ const tableBooks = function createTable(books) {
         "Title varchar(50) NOT NULL,\n" +
         "Description varchar(2000) NOT NULL,\n" +
         "ISBN int NOT NULL,\n" +
-        "Publisher varchar(50) NOT NULL,\n" +
-        // "PublisherID int NOT NULL,\n" +
+        "Publisher varchar(50),\n" +
+        //"PublisherID int,\n" +
+        "Authors varchar(150),\n" +
+        //"AuthorID int,\n" +
         "PublishedDate varchar(10) NOT NULL,\n" +
         "PageCount int NOT NULL,\n" +
         "Amount float NOT NULL,\n" +
-        "PRIMARY KEY (ID)\n" + // ,
-        // "FOREIGN KEY (PublisherID) REFERENCES Publishers (ID)\n" +
+        "PRIMARY KEY (ID) \n" +
+        //"FOREIGN KEY (PublisherID) REFERENCES Publishers (ID) ON DELETE CASCADE ON UPDATE CASCADE \n" +
+        //"FOREIGN KEY (AuthorID) REFERENCES Authors (ID) ON DELETE CASCADE ON UPDATE CASCADE \n" +
         ");";
 
         books.query(sql, (err, results, fields) => {
